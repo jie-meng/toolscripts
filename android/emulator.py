c300d93066cda1397a6b00449b3a2498c002c32a
Jie Meng <jmengxy@gmail.com>
2020-04-02 16:22:26 +0800

    emulator support -writable-system

--- android/emulator.py
+++ android/emulator.py
@@ -16,5 +16,15 @@ for a in avds:
     print('{0}. {1}'.format(idx, a))
     idx += 1
 
-selection = input()
-os.system('emulator -avd {0}'.format(avds[int(selection) - 1]))
+selection = int(input())
+emulator = avds[int(selection) - 1]
+
+print('{0} selected.\n'.format(emulator))
+
+print('start with -writable-system? (y/n)')
+writable_system = ''
+ans = input()
+if ans.lower().startswith('y'):
+    writable_system = '-writable-system'
+
+os.system('emulator -avd {0} {1}'.format(emulator, writable_system))
