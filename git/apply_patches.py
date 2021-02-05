f4f0b005e6d44ce17ab473cdb68b9fcd208db52b
Jie Meng <jmengxy@gmail.com>
2021-02-05 21:11:12 +0800

    remove shebang of kindle-pdf-cropper.py

--- git/apply_patches.py
+++ git/apply_patches.py
@@ -3,13 +3,13 @@ import os.path
 def findFiles(path, pred = None, ls = None):
     if ls == None:
         ls = []
-        
+
     for p in os.listdir(path):
         p = os.path.join(path, p)
         if os.path.isfile(p):
             if not pred or pred(p):
                 ls.append(p)
-            
+
     return ls
 
 if __name__  == '__main__':
--- image/kindle-pdf-cropper.py
+++ image/kindle-pdf-cropper.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Install dependencies on MacOS
 #
 # brew install imagemagick
