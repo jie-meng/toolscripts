f0fbd779c80faff61685c8b3a275255c2eda7c53
Jie Meng <jmengxy@gmail.com>
2021-05-25 11:01:03 +0800

    logcat-tag support applicationId

--- android/logcat-tag.py
+++ android/logcat-tag.py
@@ -27,6 +27,25 @@ def select_device():
 
     return devices[selection - 1]
 
+def select_process(device):
+    application_id = input('please input applicationId: ')
+    if len(application_id) == 0:
+        return ''
+
+    pid_info = ''
+    p = os.popen('adb -s {0} shell ps'.format(device))
+    output = p.read()
+    process_lines = list(filter(lambda x: application_id in x, output.split('\n')))
+    for process_line in process_lines:
+        format_line = '\t'.join(process_line.split())
+        items = format_line.split('\t')
+        if len(items) >= 9 and items[8] == application_id:
+            print('pid = {0}'.format(items[1]))
+            pid_info = '--pid={0}'.format(items[1])
+            break
+
+    return pid_info
+
 def main():
     device = select_device()
     if device == None:
@@ -35,14 +54,14 @@ def main():
 
     print('{0} selected.\n'.format(device))
 
-    args = ''
-    if len(sys.argv) > 1:
-        args = ' '.join(sys.argv[1:])
-    else:
-        print('please input args: (e.g. TAG1:I TAG2:D *:S)')
-        args = input().strip()
+    process_info = select_process(device)
+    print('please input args: (e.g. TAG1:I TAG2:D *:S)')
+    args = input().strip()
 
-    os.system('adb -s {0} logcat -c && adb -s {0} logcat "{1}"'.format(device, args))
+    command = 'adb -s {0} logcat -c && adb -s {0} logcat {1} "{2}"'.format(device, process_info, args)
+    print(command)
+    os.system(command)
 
 if __name__ == "__main__":
     main()
+
