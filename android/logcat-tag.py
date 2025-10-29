64225c4f2d563a367bbeba56ab07e725a5f8fb5e
Jie Meng <jmengxy@gmail.com>
2025-10-29 08:22:48 +0800

    Refactor: Deduplicate and unify Android device selection logic

--- android/logcat-tag.py
+++ android/logcat-tag.py
@@ -1,32 +1,6 @@
 import os
 import sys
-
-
-def select_device():
-    p = os.popen('adb devices')
-    output = p.read()
-    device_lines = filter(lambda x: '\t' in x, output.split('\n'))
-    devices = list(map(lambda x: x.split('\t')[0], device_lines))
-
-    if len(devices) == 0:
-        return None
-
-    if len(devices) == 1:
-        return devices[0]
-
-    print('Please select device:')
-
-    idx = 1
-    for a in devices:
-        print('{0}. {1}'.format(idx, a))
-        idx += 1
-
-    selection = int(input())
-    if selection < 1 or selection > len(devices):
-        print('Invalid selection')
-        sys.exit(-1)
-
-    return devices[selection - 1]
+from android_utils import select_device
 
 
 def select_process(device):
